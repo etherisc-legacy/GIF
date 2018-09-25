56bbf4f88fc77525381215e844b2d3904da82abb
Andrianov Konstantsin <andrianov.dev@yandex.by>
2018-09-25 16:43:39 +0300

    Configure testing

--- core_microservices/dip_event_logging/bootstrap.js
+++ core_microservices/dip_event_logging/bootstrap.js
@@ -2,7 +2,7 @@ const DipEventLogging = require('./DipEventLogging');
 
 const dipEventLogging = new DipEventLogging({
   amqpBroker: process.env.MESSAGE_BROKER || 'amqp://localhost:5672',
-  pgConnectionString: process.env.DATABASE_URL || 'postgresql://postgresql:postgresql@localhost:5432/postgresql',
+  pgConnectionString: process.env.DATABASE_URL || 'postgresql://postgresql:postgresql@localhost:5433/postgresql',
 });
 
 dipEventLogging.listen();
