ddfda9395579e8141c291ab14bcf4dfa3172deef
Dima Savitski <dimasavitski@gmail.com>
2019-02-06 14:39:45 +0000

    Merged in fix/use_demo_mode_for_fdd_web (pull request #62)
    GIF-231 Use Demo mode for Fdd.Web
    
    * Use Demo mode for Fdd.Web
    
    Approved-by: Siarhei Holub
    Approved-by: Kanstantsin Andryanau

--- app_microservices/fdd.web/src/components/testModeNotifications/testnet.js
+++ app_microservices/fdd.web/src/components/testModeNotifications/testnet.js
@@ -4,7 +4,7 @@ import styles from './styles.m.css';
 const TestModeNotification = () => (
   <div className={styles.testMode}>
     You are using a demo version of Flight Delay<br />
-    <small>Demo version works on Ethereum testnet Ropsten. Please choose it in Metamask.</small>
+    <small>Demo version works on Ethereum testnet.</small>
   </div>
 );
 
