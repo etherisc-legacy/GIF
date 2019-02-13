4d052e79619a039ac4ec00c66352fd14fdbf55e8
Kanstantsin Andryanau <k.andryanau@altoros.com>
2019-02-13 13:47:40 +0000

    Merged in feature/fix_payout_calculation2 (pull request #93)
    Fix payout calculations
    
    * Fix payout calculations
    
    * +
    
    Approved-by: Dima Savitski
    Approved-by: Siarhei Holub

--- app_microservices/fdd.web/src/shared/calculations.js
+++ app_microservices/fdd.web/src/shared/calculations.js
@@ -2,47 +2,41 @@ import { MAX_PAYOUTS } from './constants';
 
 import CustomConfig from './customConfig';
 
-const getPayout = (premium, reserved1, reserved2, w, maxPayout) => pattern => {
-  const weight = Math.floor(w * 10000) / 10000;
-  const premiumAmount = premium * (1 - reserved1) * (1 - reserved2);
-  const value = Math.min(Number((premiumAmount * pattern) / weight), maxPayout);
-  return (Math.floor(value * 100)/100).toFixed(2);
-}
 
 export const calculatePayouts = (stats) => (premium, currency) => {
   if (!premium || premium <= 0 || isNaN(Number(premium))) {
     return false;
   }
 
-  const d15 = stats.late15 / stats.observations;
-  const d30 = stats.late30 / stats.observations;
-  const d45 = stats.late45 / stats.observations;
-  const can = stats.cancelled / stats.observations;
-  const div = stats.diverted / stats.observations;
+  const weightPattern = CustomConfig.payoutCalc.weightPattern || [0, 0, 0, 30, 50, 50];
+  const maxPayouts = CustomConfig.payoutCalc.maxPayouts || MAX_PAYOUTS;
+  const MAX_PAYOUT = maxPayouts[currency];
 
-  const weightPattern = CustomConfig.payoutCalc.weightPattern || [0, 0, 30, 50, 50];
+  const statistics = [
+    stats.observations, stats.late15, stats.late30, stats.late45, stats.cancelled, stats.diverted,
+  ];
 
-  const reserved1 = 0;
-  const reserved2 = 0;
+  let weight = 0;
 
-  let weight = (d15 * weightPattern[0]) +
-    (d30 * weightPattern[1]) +
-    (d45 * weightPattern[2]) +
-    (div * weightPattern[3]) +
-    (can * weightPattern[4]);
-
-  if (weight === 0) {
-    weight = 10;
+  for (let i = 1; i < 6; i++) {
+    weight += weightPattern[i] * statistics[i] * 10000 / stats.observations;
   }
 
-  const maxPayouts = CustomConfig.payoutCalc.maxPayouts || MAX_PAYOUTS;
-  const payout = getPayout(premium, reserved1, reserved2, weight, maxPayouts[currency]);
+  if (weight === 0) {
+    weight = 100000 / stats.observations;
+  }
 
-  return {
-    p1: payout(weightPattern[0]),
-    p2: payout(weightPattern[1]),
-    p3: payout(weightPattern[2]),
-    p4: payout(weightPattern[3]),
-    p5: payout(weightPattern[4]),
-  };
+  weight = Math.floor(weight);
+
+  let payoutOptions = {};
+
+  for (let i = 0; i < 5; i++) {
+    let payout = premium * weightPattern[i + 1] * 10000 / weight;
+
+    if (payout > MAX_PAYOUT) payout = MAX_PAYOUT;
+
+    payoutOptions[`p${i+1}`] = (Math.floor(payout * 100)/100).toFixed(2);
+  }
+
+  return payoutOptions;
 };
