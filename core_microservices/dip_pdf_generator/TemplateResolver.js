69349ef2b328d5aa2b70fc44e11f58e332f4dbb8
Siarhei Holub <siarhei.holub@altoros.com>
2019-02-14 09:20:58 +0000

    Merged in feature/gif-250-and-gif-251 (pull request #100)
    GIF-250 and GIF-251
    
    * GIF-250 and GIF-251
    
    Approved-by: Dima Savitski
    Approved-by: Kanstantsin Andryanau

--- core_microservices/dip_pdf_generator/TemplateResolver.js
+++ core_microservices/dip_pdf_generator/TemplateResolver.js
@@ -16,7 +16,7 @@ class TemplateResolver {
 
     handlebars.registerHelper({
       date: value => `${moment(value).utcOffset(moment.parseZone(value).utcOffset()).format('MMMM DD, YYYY HH:mm')} (local time)`,
-      money: value => Number((Number(value) / 100).toFixed(0)),
+      money: value => Number((Number(value) / 100).toFixed(2)),
     });
   }
 
