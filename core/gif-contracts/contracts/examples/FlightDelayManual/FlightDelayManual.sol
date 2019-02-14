9423185a191da25b3bb9f65c672299320fef1db0
Kanstantsin Andryanau <k.andryanau@altoros.com>
2019-02-14 19:16:18 +0300

    refactor: FlightDelayManual refactoring

--- core/gif-contracts/contracts/examples/FlightDelayManual/FlightDelayManual.sol
+++ core/gif-contracts/contracts/examples/FlightDelayManual/FlightDelayManual.sol
@@ -1,27 +1,8 @@
 pragma solidity 0.5.2;
 
-import "../../InsuranceProduct.sol";
+import "../../Product.sol";
 
-
-contract FlightDelayManual is InsuranceProduct {
-
-    bytes32 public constant NAME = "FlightDelayManual";
-    bytes32 public constant POLICY_FLOW = "PolicyFlowDefault";
-
-    uint256 public constant MIN_OBSERVATIONS = 10;
-
-    uint8[6] public WEIGHT_PATTERN = [
-        0,
-        10, // late15
-        20, // late30
-        30, // late45
-        50, // cancelled
-        50  // diverted
-    ];
-
-    // maximum cumulated weighted premium per risk
-    uint256 constant MAX_CUMULATED_WEIGHTED_PREMIUM = 60000;
-
+contract FlightDelayManual is Product {
     event LogRequestFlightStatistics(
         uint256 requestId,
         bytes32 carrierFlightNumber,
@@ -35,14 +16,37 @@ contract FlightDelayManual is InsuranceProduct {
         uint256 arrivalTime
     );
 
-    event LogRequestPayout(
-        uint256 claimId,
-        uint256 payoutId,
-        uint256 amount
-    );
+    event LogRequestPayout(uint256 claimId, uint256 payoutId, uint256 amount);
+
+    bytes32 public constant NAME = "FlightDelayManual";
+    bytes32 public constant POLICY_FLOW = "PolicyFlowDefault";
+
+    // Minimum observations for valid prediction
+    uint256 public constant MIN_OBSERVATIONS = 10;
+    // Minimum time before departure for applying
+    uint256 public constant MIN_TIME_BEFORE_DEPARTURE = 24 hours;
+    // Maximum duration of flight
+    uint256 public constant MAX_FLIGHT_DURATION = 2 days;
+    // Check for delay after .. minutes after scheduled arrival
+    uint256 public constant CHECK_PAYOUT_OFFSET = 15 minutes;
+
+    // All amounts expected to be provided in a currency’s smallest unit
+    // E.g. 10 EUR = 1000 (1000 cents)
+    uint256 public constant MIN_PREMIUM = 1500;
+    uint256 public constant MAX_PREMIUM = 29000;
+    uint256 public constant MAX_PAYOUT = 150000;
+
+    bytes32[1] public currencies = [bytes32("EUR")];
+
+    // ['observations','late15','late30','late45','cancelled','diverted']
+    uint8[6] public weightPattern = [0, 0, 0, 30, 50, 50];
+
+    // Maximum cumulated weighted premium per risk
+    uint256 constant MAX_CUMULATED_WEIGHTED_PREMIUM = 6000000;
 
     struct Risk {
         bytes32 carrierFlightNumber;
+        bytes32 departureYearMonthDay;
         uint256 departureTime;
         uint256 arrivalTime;
         uint delayInMinutes;
@@ -58,38 +62,49 @@ contract FlightDelayManual is InsuranceProduct {
         bytes32 riskId;
     }
 
-    mapping (bytes32 => Risk) public risks;
+    mapping(bytes32 => Risk) public risks;
 
     RequestMetadata[] public requests;
 
-    constructor(address _productController) public InsuranceProduct(_productController, NAME, POLICY_FLOW) {
-        createRole("applicationManager");
-        createRole("underwriteManager");
-        createRole("payoutManager");
-
-        addRoleToAccount(msg.sender, "applicationManager");
-        addRoleToAccount(msg.sender, "underwriteManager");
-        addRoleToAccount(msg.sender, "payoutManager");
-    }
+    constructor(address _productController)
+        public
+        Product(_productController, NAME, POLICY_FLOW)
+    {}
 
     function applyForPolicy(
-        // domain specific struct
+        // domain specific
         bytes32 _carrierFlightNumber,
         uint256 _departureTime,
         uint256 _arrivalTime,
-        // premium struct
+        // premium
         uint256 _premium,
-        uint256 _currency,
+        bytes32 _currency,
         uint256[] calldata _payoutOptions,
-        // customer struct
+        // customer
         bytes32 _customerExternalId
-    ) external onlyWithRole("applicationManager") {
+    ) external {
         // Validate input parameters
-        require(_departureTime > block.timestamp, "ERROR::INVALID_DEPARTURE_TIME");
-        require(_arrivalTime > block.timestamp, "ERROR::INVALID_ARRIVAL_TIME");
+        // Validate input parameters
+        require(_premium >= MIN_PREMIUM, "ERROR::INVALID_PREMIUM");
+        require(_premium <= MAX_PREMIUM, "ERROR::INVALID_PREMIUM");
+        require(_currency == currencies[0], "ERROR:INVALID_CURRENCY");
+        require(
+            _departureTime < _departureTime,
+            "ERROR::INVALID_ARRIVAL/DEPARTURE_TIME"
+        );
+        require(
+            _departureTime > _departureTime + MAX_FLIGHT_DURATION,
+            "ERROR::INVALID_ARRIVAL/DEPARTURE_TIME"
+        );
+        require(
+            _departureTime < block.timestamp + MIN_TIME_BEFORE_DEPARTURE,
+            "ERROR::INVALID_ARRIVAL/DEPARTURE_TIME"
+        );
 
         // Create risk if not exists
-        bytes32 riskId = keccak256(abi.encodePacked(_carrierFlightNumber, _departureTime, _arrivalTime));
+        bytes32 riskId = keccak256(
+            abi.encodePacked(_carrierFlightNumber, _departureTime, _arrivalTime)
+        );
         Risk storage risk = risks[riskId];
 
         if (risk.carrierFlightNumber == "") {
@@ -108,7 +123,12 @@ contract FlightDelayManual is InsuranceProduct {
         }
 
         // Create new application
-        uint256 applicationId = newApplication(_customerExternalId, _premium, _currency, _payoutOptions);
+        uint256 applicationId = newApplication(
+            _customerExternalId,
+            _premium,
+            _currency,
+            _payoutOptions
+        );
 
         // New request
         uint256 requestId = requests.length++;
@@ -116,10 +136,18 @@ contract FlightDelayManual is InsuranceProduct {
         requestMetadata.applicationId = applicationId;
         requestMetadata.riskId = riskId;
 
-        emit LogRequestFlightStatistics(requestId, _carrierFlightNumber, _departureTime, _arrivalTime);
+        emit LogRequestFlightStatistics(
+            requestId,
+            _carrierFlightNumber,
+            _departureTime,
+            _arrivalTime
+        );
     }
 
-    function flightStatisticsCallback(uint256 requestId, uint256[6] calldata _statistics) external onlyWithRole("underwriteManager") {
+    function flightStatisticsCallback(
+        uint256 requestId,
+        uint256[6] calldata _statistics
+    ) external {
         // Statistics: ['observations','late15','late30','late45','cancelled','diverted']
 
         uint256 applicationId = requests[requestId].applicationId;
@@ -129,31 +157,34 @@ contract FlightDelayManual is InsuranceProduct {
             return;
         }
 
-        uint256 weight;
-        for (uint256 i = 1; i <= 5; i++) {
-            weight += WEIGHT_PATTERN[i] * _statistics[i] * 10000 / _statistics[0];
-            // 1% = 100 / 100% = 10,000
-        }
-
-        // to avoid div0 in the payout section,
-        // we have to make a minimal assumption on weight.
-        if (weight == 0) {
-            weight = 100000 / _statistics[0];
-        }
-
         uint256 premium = getPremium(applicationId);
-         uint256[] memory payoutOptions = getPayoutOptions(applicationId);
-         for (uint256 i = 0; i <= 4; i++) {
-            // todo: Use real values in newApplication
-            require(payoutOptions[i] == premium * WEIGHT_PATTERN[i + 1] * 10000 / weight, "ERROR::INVALID_PAYOUT_OPTION");
-         }
+        uint256[] memory payoutOptions = getPayoutOptions(applicationId);
+        (uint256 weight, uint256[5] memory calculatedPayouts) = calculatePayouts(
+            premium,
+            _statistics
+        );
+
+        require(
+            payoutOptions.length == calculatedPayouts.length,
+            "ERROR::INVALID_PAYOUT_OPTIONS_COUNT"
+        );
+
+        for (uint256 i = 0; i < 5; i++) {
+            require(
+                payoutOptions[i] == calculatedPayouts[i],
+                "ERROR::INVALID_PAYOUT_OPTION"
+            );
+            assert(payoutOptions[i] <= MAX_PAYOUT);
+        }
 
         bytes32 riskId = requests[requestId].riskId;
 
         if (risks[riskId].premiumMultiplier == 0) {
             // it's the first policy for this risk, we accept any premium
-            risks[riskId].cumulatedWeightedPremium = premium * 10000;
+            risks[riskId].cumulatedWeightedPremium = premium * 100000;
             risks[riskId].premiumMultiplier = 100000 / weight;
+        } else {
+            risks[riskId].cumulatedWeightedPremium = risks[riskId].cumulatedWeightedPremium + premium * risks[riskId].premiumMultiplier;
         }
 
         risks[riskId].weight = weight;
@@ -165,10 +196,19 @@ contract FlightDelayManual is InsuranceProduct {
         RequestMetadata storage requestMetadata = requests[newRequestId];
         requestMetadata.policyId = policyId;
 
-        emit LogRequestFlightStatus(newRequestId, risks[riskId].carrierFlightNumber, risks[riskId].arrivalTime);
+        emit LogRequestFlightStatus(
+            newRequestId,
+            risks[riskId].carrierFlightNumber,
+            risks[riskId].arrivalTime
+        );
     }
 
-    function flightStatusCallback(uint256 requestId, uint256 _delay, bool _cancelled, bool _diverted) external onlyWithRole("underwriteManager") {
+    function flightStatusCallback(
+        uint256 requestId,
+        uint256 _delay,
+        bool _cancelled,
+        bool _diverted
+    ) external {
         uint256 policyId = requests[requestId].policyId;
         uint256 applicationId = requests[requestId].policyId;
         uint256[] memory payoutOptions = getPayoutOptions(applicationId);
@@ -197,7 +237,35 @@ contract FlightDelayManual is InsuranceProduct {
         }
     }
 
-    function confirmPayout(uint256 _payoutId, uint256 _sum) external onlyWithRole("payoutManager") {
+    function confirmPayout(uint256 _payoutId, uint256 _sum) external {
         payout(_payoutId, _sum);
+    }
+
+    function calculatePayouts(uint256 _premium, uint256[6] memory _statistics)
+        public
+        view
+        returns (uint256 _weight, uint256[5] memory _payoutOptions)
+    {
+        require(_premium >= MIN_PREMIUM, "ERROR::INVALID_PREMIUM");
+        require(_premium <= MAX_PREMIUM, "ERROR::INVALID_PREMIUM");
+        require(_statistics[0] > MIN_OBSERVATIONS, "ERROR::LOW_OBSERVATIONS");
+
+        for (uint256 i = 1; i < 6; i++) {
+            _weight += weightPattern[i] * _statistics[i] * 10000 / _statistics[0];
+            // 1% = 100 / 100% = 10,000
+        }
+
+        // To avoid div0 in the payout section, we have to make a minimal assumption on weight
+        if (_weight == 0) {
+            _weight = 100000 / _statistics[0];
+        }
+
+        for (uint256 i = 0; i < 5; i++) {
+            _payoutOptions[i] = _premium * weightPattern[i + 1] * 10000 / _weight;
+
+            if (_payoutOptions[i] > MAX_PAYOUT) {
+                _payoutOptions[i] = MAX_PAYOUT;
+            }
+        }
     }
 }
