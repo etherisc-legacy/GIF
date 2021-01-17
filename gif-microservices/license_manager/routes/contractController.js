c53b69d719d3189160f50da28ad868cbcbd1e7f0
Christoph Mussenbrock <christoph@etherisc.com>
2021-01-17 02:25:21 +0100

    Call Contract via api

--- gif-microservices/license_manager/routes/contractController.js
+++ gif-microservices/license_manager/routes/contractController.js
@@ -9,16 +9,25 @@ const getArtifactSchema = {
   additionalProperties: false,
 };
 
+const contractCallSchema = {
+  properties: {
+    product: { type: 'string' },
+    contractName: { type: 'string' },
+    methodName: { type: 'string' },
+    parameters: { type: 'object' },
+  },
+  required: ['product', 'contractName', 'methodName', 'parameters'],
+  additionalProperties: false,
+};
+
 
 module.exports = async ({
   ajv,
   router,
-  log,
   gifService,
 }) => {
   // Get Contract Artifact
-  router.get('/api/artifact/get', async (ctx, data) => {
-    console.log(ctx.request.body);
+  router.get('/api/artifact/get', async (ctx) => {
     const validate = ajv.compile(getArtifactSchema);
     if (!validate(ctx.request.body)) {
       ctx.badRequest({ error: validate.errors });
@@ -26,6 +35,18 @@ module.exports = async ({
     }
 
     const artifact = await gifService.getArtifact(ctx.request.body);
+
+    ctx.ok(artifact);
+  });
+
+  router.get('/api/contract/call', async (ctx) => {
+    const validate = ajv.compile(contractCallSchema);
+    if (!validate(ctx.request.body)) {
+      ctx.badRequest({ error: validate.errors });
+      return;
+    }
+
+    const artifact = await gifService.callContract(ctx.request.body);
 
     ctx.ok(artifact);
   });
